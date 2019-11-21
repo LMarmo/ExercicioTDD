43311d6ee0e4302970cf0e3690084e2215f25f30
Lucas <lukasmarmo@gmail.com>
2019-11-20 21:27:32 -0300

    (1)

---
+++ src/Teste.java
@@ -0,0 +1,17 @@
+
+
+
+/**
+ *
+ * @author lukas
+ */
+public class Teste {
+    
+     public void testMultiplication() {
+       Dollar five = new Dollar(5);
+       five.times(2);
+       assertEquals(10, five.amount);
+    }
+     
+     
+}
